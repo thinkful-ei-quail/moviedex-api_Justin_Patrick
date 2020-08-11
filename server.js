89111fc998caab3dd14c4ffa422463e72365cb60
Patrick Easter <epatri6@gmail.com>
2020-08-11 15:33:40 -0400

    Genre and Country Search Done

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const Movies = require('./movies-data-small.json')
+const Movies = require('./movies-data-small.json');
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
+app.use(morgan('dev'));
+app.use(cors());
 
 // Users can search for Movies by genre, country or avg_vote
 
 
 const handleGetMovie = (req, res) => {
-  results = Movies;
-  console.log("Inside handle Movie");
-  results = handleGetGenre(req.genre, results);
-  results = handleGetCountry(req.country, results);
-  results = handleGetVote(req.avg_vote, results);
+  const {genre, country, avg_vote} = req.query;
+  let results = Movies;
+  results = handleGetGenre(genre, results);
+  results = handleGetCountry(country, results);
+  results = handleGetVote(avg_vote, results);
 
-  res.send(results)
-}
+  res.send(results);
+};
 
-app.get('/movie', handleGetMovie)
+const handleGetGenre = (genre, movies) => {
+  genre = (genre ? genre.toLowerCase() : '');
+  return movies.filter(movie => movie['genre'] && movie['genre'].toLowerCase().includes(genre));
+};
 
-app.listen(8000, () => {})
+const handleGetCountry = (country, movies) => {
+  country = (country ? country.toLowerCase() : '');
+  return movies.filter(movie => movie['country'] && movie['country'].toLowerCase().includes(country));
+};
+
+const handleGetVote = (avgVote, movies) => {
+  return movies;
+};
+
+app.get('/movie', handleGetMovie);
+
+app.listen(8000, () => {});
