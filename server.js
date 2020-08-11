0f637b34593e2d8b23b875ce1b28b25870c38644
Justin Foster <jfoster91@gmail.com>
2020-08-11 16:16:14 -0400

    Just need to finish validateUser

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -8,7 +9,18 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 
-// Users can search for Movies by genre, country or avg_vote
+app.use(validateUser);
+
+const validateUser = (req, res, next) => {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
+  next()
+}
 
 
 const handleGetMovie = (req, res) => {
@@ -22,17 +34,30 @@ const handleGetMovie = (req, res) => {
 };
 
 const handleGetGenre = (genre, movies) => {
-  genre = (genre ? genre.toLowerCase() : '');
+  if(!genre){
+    return movies;
+  }
+  genre = genre.toLowerCase();
   return movies.filter(movie => movie['genre'] && movie['genre'].toLowerCase().includes(genre));
 };
 
 const handleGetCountry = (country, movies) => {
-  country = (country ? country.toLowerCase() : '');
+  if(!country){
+    return movies;
+  }
+  country = country.toLowerCase();
   return movies.filter(movie => movie['country'] && movie['country'].toLowerCase().includes(country));
 };
 
 const handleGetVote = (avgVote, movies) => {
-  return movies;
+  if(!avgVote){
+    return movies;
+  } 
+  avgVote = parseInt(avgVote)
+  if(isNaN(avgVote)) {
+    return movies;
+  }
+  return movies.filter(movie => movie['avg_vote'] && movie['avg_vote'] >= avgVote)
 };
 
 app.get('/movie', handleGetMovie);
